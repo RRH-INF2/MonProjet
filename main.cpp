866d0466ae4570b1ff5c3100fab2e7185f699106
User2 <rene.rentsch@hotmail.com>
2019-03-28 20:57:53 +0100

    fichier header.cpp complété

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main() {
    cout << "Message 1 de user1" << endl;
    cout << "Message 2 de user1" << endl;
    cout << "Message 3 de user1" << endl;
+   cout << "Message 1 de user2" << endl;
    cout << "Message 2 de user2" << endl;
    return EXIT_SUCCESS;
 }
