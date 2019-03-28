68b3b300b1ba9e8a4aaea24dc9e8f6149710187a
User1 <rene.rentsch@hotmail.com>
2019-03-28 19:39:52 +0100

    fichier header.cpp complété

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main() {
    cout << "Message 1 de user1" << endl;
    cout << "Message 2 de user1" << endl;
+   cout << "Message 3 de user1" << endl;
    return EXIT_SUCCESS;
 }
 
