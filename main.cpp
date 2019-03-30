8a92ab2873c4f8ce4a96dcf9dbb120fe0b6dc4be
StellusGit <estelle.rentsch@hotmail.com>
2019-03-30 22:43:24 +0100

    Merge origin/master
    Conflicts:
    	fichier.h
    	main.cpp

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int main() {
    cout << "1 from StellusGit" << endl;
    cout << "2 from RRH-INF1" << endl;
    cout << "3 from RRH-INF1" << endl;
+   cout << "2 from StellusGit" << endl;
    
    return EXIT_SUCCESS;
 }
