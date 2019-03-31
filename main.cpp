9854c79f0cb7c2a03d1addeea64894140ec5b54d
Estelle <estelle.rentsch@hotmail.com>
2019-03-31 18:12:22 +0200

    fds

--- main.cpp
+++ main.cpp
@@ -8,5 +8,6 @@ int main() {
    cout << "message 2 from StellusGit" << endl;
    cout << "message 1 from RRH-INF2" << endl;   
    cout << "message 2 from RRH-INF2" << endl;   
+   cout << "message 3 from StellusGit" << endl;   
    return EXIT_SUCCESS;
 }
