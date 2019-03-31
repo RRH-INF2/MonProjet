1f188a482d466febb7ace9721dd16e53aeb02e60
RRH-INF2 <rene.rentsch@hotmail.com>
2019-03-31 18:14:40 +0200

    Premier commit

--- main.cpp
+++ main.cpp
@@ -4,10 +4,5 @@
 using namespace std;
 
 int main() { 
-   cout << "message 1 from StellusGit" << endl;
-   cout << "message 2 from StellusGit" << endl;
-   cout << "message 1 from RRH-INF2" << endl;   
-   cout << "message 2 from RRH-INF2" << endl;   
-   cout << "message 3 from StellusGit" << endl;   
    return EXIT_SUCCESS;
 }
