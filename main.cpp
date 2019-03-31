92e556d9a24ef8b33937730327c5161da7d059a6
RRH-INF2 <rene.rentsch@hotmail.com>
2019-03-31 17:37:27 +0200

    Modif de fichier2.h

--- main.cpp
+++ main.cpp
@@ -7,5 +7,6 @@ int main() {
    cout << "message 1 from StellusGit" << endl;
    cout << "message 2 from StellusGit" << endl;
    cout << "message 1 from RRH-INF2" << endl;   
+   cout << "message 2 from RRH-INF2" << endl;   
    return EXIT_SUCCESS;
 }
