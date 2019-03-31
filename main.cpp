2b306b19ab63252450fadd900bac385f9cf8d37c
RRH-INF2 <rene.rentsch@hotmail.com>
2019-03-31 16:53:09 +0200

    blabla

--- main.cpp
+++ main.cpp
@@ -6,5 +6,6 @@ using namespace std;
 int main() { 
    cout << "message 1 from StellusGit" << endl;
    cout << "message 2 from StellusGit" << endl;
+   cout << "message 1 from RRH-INF2" << endl;   
    return EXIT_SUCCESS;
 }
